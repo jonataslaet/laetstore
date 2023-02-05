c7658785a9e4da861d4ecea4a34672fab986e928
jonataslaet <jonataslaetprogramador@gmail.com>
2023-02-04 23:05:34 -0300

    Creation of project and development of entities
    Creating project
    Developing entities Product and ProductCategory

---
+++ backend-java/src/main/java/com/github/jonataslaet/laetstore/LaetstoreApplication.java
@@ -0,0 +1,13 @@
+package com.github.jonataslaet.laetstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaetstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaetstoreApplication.class, args);
+	}
+
+}
---
+++ backend-java/src/test/java/com/github/jonataslaet/laetstore/LaetstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.jonataslaet.laetstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaetstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
