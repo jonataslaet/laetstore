5bc2b5b45c5d46509a838cb4d8cf7df3f5c55423
jonataslaet <jonataslaetprogramador@gmail.com>
2023-02-05 08:31:23 -0300

    Creation of REST APIs
    Creating REST APIs products and product-category
    Creating population of data

---
+++ backend-java/src/main/java/com/github/jonataslaet/laetstore/repository/ProductCategoryRepository.java
@@ -0,0 +1,9 @@
+package com.github.jonataslaet.laetstore.repository;
+
+import com.github.jonataslaet.laetstore.entity.ProductCategory;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
+public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
+}
